25fb214f88ce107462eaba29bd218412a3e133a6
Raj Mehta <49105619+rajmehtajj@users.noreply.github.com>
2023-08-06 19:46:43 -0600

    Time: 161 ms (81.96%), Space: 16.7 MB (91.76%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -3,10 +3,8 @@ class Solution:
         size = len(flowerbed)
         count = 0
         i = 0
-        
         while i < size:
             if flowerbed[i] == 0:
-                # Check if the previous and next plots are also empty or out of bounds
                 prev_empty = (i == 0 or flowerbed[i - 1] == 0)
                 next_empty = (i == size - 1 or flowerbed[i + 1] == 0)
                 
@@ -14,5 +12,4 @@ class Solution:
                     flowerbed[i] = 1  # Place a flower in the current plot
                     count += 1
             i += 1
-            
         return count >= n
