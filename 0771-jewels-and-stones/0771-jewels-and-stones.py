e495cf934ebf4087c8593f81352aacbfe3eed18a
Raj Mehta <49105619+rajmehtajj@users.noreply.github.com>
2023-10-17 09:06:14 -0600

    Time: 32 ms (90.92%), Space: 16.1 MB (76.02%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,12 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        # Initialize a variable to count the number of jewels in stones
+ 
         num_jewels = 0
 
-        # Create a set of jewel types for efficient lookup
         jewel_set = set(jewels)
 
-        # Iterate through each stone
         for stone in stones:
             if stone in jewel_set:
                 # If the stone is a jewel, increment the count
