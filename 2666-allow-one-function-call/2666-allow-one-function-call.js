6e3cfea477b3f021c1fbae68f526a6819f8bb5d5
Raj Mehta <49105619+rajmehtajj@users.noreply.github.com>
2023-10-21 12:06:07 -0600

    Time: 59 ms (13.94%), Space: 42.3 MB (11.09%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,14 @@
+var once = function(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            result = fn(...args);
+            called = true;
+            return result;
+        } else {
+            return undefined;
+        }
+    }
+};
